4944f5f938095d225f17cc0ebc7e291cf040ec87
Jerry(이재헌) <ti6419@gmail.com>
2022-12-14 08:49:38 +0900

    feat: GET /posts/daily-post-counts 추가

--- src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
+++ src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -1,20 +1,29 @@
 package com.example.fastcampusmysql.application.controller;
 
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.dto.PostRequest;
+import com.example.fastcampusmysql.domain.post.service.PostReadService;
 import com.example.fastcampusmysql.domain.post.service.PostWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/posts")
 public class PostController {
     private final PostWriteService postWriteService;
+    private final PostReadService postReadService;
 
     @PostMapping
     public Long create(PostRequest request) {
         return postWriteService.create(request);
+    }
+
+    @GetMapping("/daily-post-counts")
+    public List<DailyPostCount> getDailyPostCounts(@RequestBody DailyPostCountRequest request) {
+        return postReadService.getDailyPostCounts(request);
     }
 }
